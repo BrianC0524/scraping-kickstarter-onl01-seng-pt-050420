c4d77a57eeb4bc84500b9425bb56ac2d849c98d5
Brian Craycraft <craycraftb24@hotmail.com>
2020-06-24 18:27:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,14 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  
+  # projects: kickstarter.css("li.project.grid_4")
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
